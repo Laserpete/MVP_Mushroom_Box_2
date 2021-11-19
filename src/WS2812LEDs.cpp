bc6ed5a693c7814d411411db57334d20cd14ae05
Laserpete <pete.t.doyle@gmail.com>
2021-11-19 08:31:29 +0100

    Add pretty lights using FastLED

--- src/WS2812LEDs.cpp
+++ src/WS2812LEDs.cpp
@@ -1,5 +1,47 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
 #include "header.h"
 
-void setupWS2812LEDs();
+CRGB leds[NUM_LEDS];
+
+CRGBPalette16 currentPallette;
+TBlendType currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemRGBPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+void setupWS2812LEDs() {
+  FastLED.addLeds<LED_TYPE, WS2812_PIN, COLOUR_ORDER>(leds, NUM_LEDS)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  currentPallette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
+}
+
+void whatToDisplayOnLEDs(SensorData SensorData, bool humidifierIsOn) {
+  if (humidifierIsOn == 1) {
+    humidifyLightshowLEDs();
+  }
+}
+
+void humidifyLightshowLEDs() {
+  ChangePalettPeriodically();
+
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1;  // motion speed
+
+  FillLEDsFromPaletteColours(startIndex);
+  FastLED.show();
+}
+
+void FillLEDsFromPaletteColours(uint8_t colourIndex) {
+  uint8_t brightness = 255;
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    leds[i] = ColorFromPalette(currentPallette, colourIndex, brightness,
+                               currentBlending);
+    colourIndex += 3;
+  }
+}
+
+void ChangePalettePeriodically() {}
